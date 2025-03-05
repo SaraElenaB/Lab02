f8f219e6fb66eabc0c7ee171909b908cbd9997c0
SaraElenaB <sara.bellotti03@gmail.com>
2025-03-05 18:40:08 +0100

    impostazione esercizio 1

--- dictionary.py
+++ dictionary.py
@@ -1,12 +1,29 @@
 class Dictionary:
+
     def __init__(self):
-        pass
+        self.dizionario = {}
 
-    def addWord(self):
-        pass
+    def addWord(self, parolaAliena, parolaTradotta):
+        self.dizionario[parolaAliena.lower()] = parolaTradotta
 
-    def translate(self):
-        pass
+    def translate(self, parolaAliena):
+        if parolaAliena.lower() in self.dizionario.keys():
+            return self.dizionario[parolaAliena.lower()]
 
-    def translateWordWildCard(self):
-        pass
+    def translateWordWildCard(self, parolaAliena):
+        for parola in self.dizionario.keys():
+            if parola==parolaAliena.lower():
+                return self.dizionario[parolaAliena]
+        print("La parola aliena non esiste o non è ancora stata tradotta! Riprovare")
+        return None
+        # attenzione, non va bene scrivere if: else: perchè vuol dire che se alla prima iterazione l'if
+        # non è verificato stampa subito l'else. perciò mettilo fuori dal ciclo di for
+
+    def printDictionary(self):
+        if len(self.dizionario) == 0:
+            print("Errore di stampa. Dizionario Vuoto!")
+        else:
+            for parolaAliena in self.dizionario.keys():
+                parolaTradotta = self.dizionario[parolaAliena]
+                print(f"{parolaAliena} - {parolaTradotta}")
+            #for parolaAliena, parolaTradotta in self.dizionario.items(): --> itera su tutti gli elementi, sia chiave che valore
--- translator.py
+++ translator.py
@@ -1,27 +1,62 @@
 class Translator:
 
     def __init__(self):
-        pass
+        self.dizionario = {}
 
+#-------------------------------------------------------------------------------------------
     def printMenu(self):
-        # 1. Aggiungi nuova parola
-        # 2. Cerca una traduzione
-        # 3. Cerca con wildcard
-        # 4. Exit
-        pass
+        print("-------------------------------------------")
+        print("    Translator Alien-Italian")
+        print("-------------------------------------------")
+        print(" 1. Aggiungi nuova parola")
+        print(" 2. Cerca una traduzione")
+        print(" 3. Cerca con wildcard")
+        print(" 4. Stampa tutto il dizionario")
+        print(" 5. Exit")
+        print("-------------------------------------------")
 
-    def loadDictionary(self, dict):
-        # dict is a string with the filename of the dictionary
-        pass
+# -------------------------------------------------------------------------------------------
+    def loadDictionary(self, filename):
 
+        dict = {}
+        try:
+            with open(filename, "r", encoding="UTF-8") as file:
+                for line in file:
+                    campi = line.strip().split(" ", 1) #1--> dividi solo una volta
+                    if len(campi) == 2:
+                        dict.addWord(campi[0], campi[1])
+        except FileNotFoundError:
+            print("Il dizionario non è stato trovato!")
+
+# -------------------------------------------------------------------------------------------
     def handleAdd(self, entry):
-        # entry is a tuple <parola_aliena> <traduzione1 traduzione2 ...>
-        pass
+         """
+         tupla --> 2 elementi insieme, già loro mi dicono che entry è una tupla di a e b
+         :param entry: tupla
+         :return:
+         """
+         parolaAliena, parolaTradotta = entry
+         if parolaAliena.isAlpha & parolaTradotta.isAlpha:
+             self.dizionario.addWord(parolaAliena, parolaTradotta)
+         else:
+             print("\nErrore: sono ammessi solo caratteri alfabetici.")
 
-    def handleTranslate(self, query):
-        # query is a string <parola_aliena>
-        pass
+# -------------------------------------------------------------------------------------------
+    def handleTranslate(self, parolaAliena):
+        #query era = a parolaAliena
+        traduzione = self.dizionario.translate(parolaAliena)
+        if traduzione is None:
+            print("La traduzione non è ancora stata trovata! ")
+            return None
+        print(f"{traduzione}+.n")
+        print(self.dizionario[parolaAliena]) #boo
 
+    # -------------------------------------------------------------------------------------------
     def handleWildCard(self,query):
         # query is a string with a ? --> <par?la_aliena>
-        pass
+        aliena = self.dizionario.translateWordWildCard(query)
+        if aliena is None:
+            print("La parola aliena non è ancora stata trovata! ")
+            return None
+        print(f"{aliena}+.n")
+        print(self.dizionario.get(aliena))  # boo
